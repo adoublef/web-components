efc990e34fa042ce9728779906f184151d76bc65
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-09-14 20:25:05 +0100

    update with example of slot feature

--- README.md
+++ README.md
@@ -7,3 +7,15 @@
 - [mfreed7/declarative-shadow-dom](https://github.com/mfreed7/declarative-shadow-dom)
 
 - [declarative shadow dom](https://developer.chrome.com/articles/declarative-shadow-dom/)
+
+- [urls and fetching](https://html.spec.whatwg.org/multipage/urls-and-fetching.html)
+
+- [context api mixin](https://github.com/blikblum/wc-context/tree/master)
+
+- [context protocol proposal](https://github.com/webcomponents-cg/community-protocols/blob/main/proposals/context.md)
+
+- [lit-labs/context](https://github.com/lit/lit/blob/main/packages/labs/context/src/lib/context-request-event.ts)
+
+- [gund/game-companion](https://github.com/gund/game-companion/pull/1/files#diff-7bb92e72516f321ce518fcc5cbc4de4e897d2c6791ea80dcc5db4eca5fcfa3ef)
+
+- [dependency injection](https://www.thinktecture.com/en/web-components/dependency-injection/)
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import { actionable, forable, loadable, createTargetable } from "./lib";
+import { actionable, forable, loadable, createTargetable, createProvidable, createSlottable } from "./lib";
+import { ContextEvent, createContext } from "./lib/context";
 
 interface Constructor<T> {
     new(...args: any[]): T;
@@ -46,3 +47,67 @@ export class EffectPluginElement extends HTMLElement {
     }
 }
 customElements.define("effect-plugin", EffectPluginElement);
+/* ******************************************************************** */
+const { slottable, slot } = createSlottable();
+@slottable
+@targetable
+class HelloWorldElement extends HTMLElement {
+    @target
+    declare span: HTMLSpanElement;
+
+    @slot
+    declare greeting: HTMLSlotElement;
+
+    connectedCallback() {
+        this.span.textContent = `${this.greeting.assignedNodes().length}`;
+    }
+}
+customElements.define("hello-world", HelloWorldElement);
+/* ******************************************************************** */
+const { providable } = createProvidable();
+
+const userIdContext = createContext("user-id", "123");
+let provideAlready = false;
+
+/* 
+https://github.com/blikblum/wc-context/blob/master/core.js
+https://github.com/blikblum/wc-context/blob/master/mixin.js
+*/
+
+@providable
+export class UserRowElement extends HTMLElement {
+    userId = "123";
+
+    connectedCallback() {
+        const ctx = new ContextEvent(userIdContext, (userId, unsubscribe) => {
+            if (!provideAlready) {
+                // this.my = userId;
+            }
+            unsubscribe?.();
+        }, true);
+        this.dispatchEvent(ctx);
+    }
+}
+customElements.define("user-row", UserRowElement);
+@providable
+@actionable
+export class FollowUserElement extends HTMLElement {
+    followUser() {
+        console.log("follow");
+    }
+
+    connectedCallback() {
+    }
+}
+customElements.define("follow-user", FollowUserElement);
+@providable
+@actionable
+export class BlockUserElement extends HTMLElement {
+    blockUser() {
+        console.log("block");
+    }
+
+    connectedCallback() {
+    }
+}
+customElements.define("block-user", BlockUserElement);
